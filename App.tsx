1c9089844c6bbf3714b6b69ff90c6959903e30db
ruchirajkarki <ruchirajkarki@gmail.com>
2025-04-15 22:04:49 +0545

    add: add navbar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import { Navbar } from 'components/Navbar';
-import { SafeAreaView, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 import './global.css';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView>
-        <Text>Hello</Text>
-        <Navbar />
-      </SafeAreaView>
+      <Navbar />
+      <StatusBar translucent />
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { Image, ImageStyle, Switch, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const Navbar = () => {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View>
-      <Text>Navbar</Text>
-    </View>
+    <SafeAreaView className="bg-red-300 dark:bg-green-600">
+      <View className="px-4">
+        <Image
+          source={require('../assets/col-graphics/euphoria-logo-light.png')}
+          style={imageStyles}
+        />
+        <Switch value={colorScheme === 'dark'} onValueChange={toggleColorScheme} />
+        <Text>{colorScheme}</Text>
+      </View>
+    </SafeAreaView>
   );
+};
+
+const imageStyles: ImageStyle = {
+  height: 100,
+  width: 120,
+  objectFit: 'contain',
 };
